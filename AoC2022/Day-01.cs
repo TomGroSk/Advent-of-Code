bb7b8c2a9374bfcbb1adedc75a136379458d9e1a
Tomasz Gromadzki-Skutnik <tomasz.gromadzki-skutnik@predicagroup.com>
2022-12-07 21:35:02 +0100

    Implementation of Day 1

---
+++ AoC2022/Day-01.cs
@@ -0,0 +1,49 @@
+﻿namespace AoC2022;
+
+public static class Day_01
+{
+    public static void part01(string data)
+    {
+        var elfCaloriesPackages = data.Split("\n\r\n");
+        var maxSum = int.MinValue;
+        foreach(var pack in elfCaloriesPackages)
+        {
+            var t = pack.Split("\n");
+            var sum = 0;
+
+            foreach(var calories in t)
+            {
+                var cals = int.Parse(calories);
+                sum += cals;
+            }
+
+            if(maxSum < sum)
+            {
+                maxSum = sum;
+            }
+        }
+
+        Console.WriteLine(maxSum);
+    }
+
+    public static void part02(string data)
+    {
+        var elfCaloriesPackages = data.Split("\n\r\n");
+        List<int> elfCaloriesSums = new();
+        foreach (var pack in elfCaloriesPackages)
+        {
+            var t = pack.Split("\n");
+            var sum = 0;
+
+            foreach (var calories in t)
+            {
+                var cals = int.Parse(calories);
+                sum += cals;
+            }
+
+            elfCaloriesSums.Add(sum);
+        }
+
+        Console.WriteLine(elfCaloriesSums.OrderByDescending(x => x).Take(3).Sum());
+    }
+}
---
+++ AoC2022/Program.cs
@@ -0,0 +1,4 @@
+﻿using AoC2022;
+using AoC2022.InputData;
+
+AoC2022.Day_01.part02(AoC2022.InputData.Day_01.TestData);
