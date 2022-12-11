e84c41bd7181b52c2020e7cf982d6acfb45be652
Tomasz Gromadzki-Skutnik <tomek.gromadzki.sk@gmail.com>
2022-12-11 12:50:54 +0100

    Implementation of day 8

---
+++ AoC2022/Day-08.cs
@@ -0,0 +1,162 @@
+﻿namespace AoC2022;
+
+public static class Day_08
+{
+    public static void part01(string data)
+    {
+        var forest = data.Split('\n', StringSplitOptions.TrimEntries);
+        int visibleForestCounter = 0;
+        for (int y = 0; y < forest.Length; y++)
+        {
+            for (int x = 0; x < forest[0].Length; x++)
+            {
+                if (x == 0 || y == 0 || x == forest[0].Length - 1 || y == forest.Length - 1)
+                {
+                    visibleForestCounter++;
+                    continue;
+                }
+
+                bool isVisible = true;
+
+                int tempCoordinate = x - 1;
+                while (tempCoordinate >= 0)
+                {
+                    if (int.Parse(forest[x][y].ToString()) <= int.Parse(forest[tempCoordinate][y].ToString()))
+                    {
+                        isVisible = false;
+                    }
+                    tempCoordinate--;
+                }
+                if (isVisible)
+                {
+                    visibleForestCounter++;
+                    continue;
+                }
+
+                isVisible = true;
+                tempCoordinate = x + 1;
+                while (tempCoordinate <= forest[0].Length - 1)
+                {
+                    if (int.Parse(forest[x][y].ToString()) <= int.Parse(forest[tempCoordinate][y].ToString()))
+                    {
+                        isVisible = false;
+                    }
+                    tempCoordinate++;
+                }
+                if (isVisible)
+                {
+                    visibleForestCounter++;
+                    continue;
+                }
+
+                isVisible = true;
+                tempCoordinate = y - 1;
+                while (tempCoordinate >= 0)
+                {
+                    if (int.Parse(forest[x][y].ToString()) <= int.Parse(forest[x][tempCoordinate].ToString()))
+                    {
+                        isVisible = false;
+                    }
+                    tempCoordinate--;
+                }
+                if (isVisible)
+                {
+                    visibleForestCounter++;
+                    continue;
+                }
+
+                isVisible = true;
+                tempCoordinate = y + 1;
+                while (tempCoordinate <= forest.Length - 1)
+                {
+                    if (int.Parse(forest[x][y].ToString()) <= int.Parse(forest[x][tempCoordinate].ToString()))
+                    {
+                        isVisible = false;
+                    }
+                    tempCoordinate++;
+                }
+                if (isVisible)
+                {
+                    visibleForestCounter++;
+                    continue;
+                }
+            }
+        }
+
+        Console.WriteLine(visibleForestCounter);
+    }
+
+    public static void part02(string data)
+    {
+        var forest = data.Split('\n', StringSplitOptions.TrimEntries);
+        List<int> scenicScore = new();
+
+        for (int y = 0; y < forest.Length; y++)
+        {
+            for (int x = 0; x < forest[0].Length; x++)
+            {
+                if (x == 0 || y == 0 || x == forest[0].Length - 1 || y == forest.Length - 1)
+                {
+                    continue;
+                }
+
+                int tempScenicScore = 0;
+                int score = 1;
+                int tempCoordinate = x - 1;
+                while (tempCoordinate >= 0)
+                {
+                    tempScenicScore++;
+                    if (int.Parse(forest[x][y].ToString()) <= int.Parse(forest[tempCoordinate][y].ToString()))
+                    {
+                        break;
+                    }
+                    tempCoordinate--;
+                }
+
+                score *= tempScenicScore;
+                tempScenicScore = 0;
+                tempCoordinate = x + 1;
+                while (tempCoordinate <= forest[0].Length - 1)
+                {
+                    tempScenicScore++;
+                    if (int.Parse(forest[x][y].ToString()) <= int.Parse(forest[tempCoordinate][y].ToString()))
+                    {
+                        break;
+                    }
+                    tempCoordinate++;
+                }
+
+                score *= tempScenicScore;
+                tempScenicScore = 0;
+                tempCoordinate = y - 1;
+                while (tempCoordinate >= 0)
+                {
+                    tempScenicScore++;
+                    if (int.Parse(forest[x][y].ToString()) <= int.Parse(forest[x][tempCoordinate].ToString()))
+                    {
+                        break;
+                    }
+                    tempCoordinate--;
+                }
+
+                score *= tempScenicScore;
+                tempScenicScore = 0;
+                tempCoordinate = y + 1;
+                while (tempCoordinate <= forest.Length - 1)
+                {
+                    tempScenicScore++;
+                    if (int.Parse(forest[x][y].ToString()) <= int.Parse(forest[x][tempCoordinate].ToString()))
+                    {
+                        break;
+                    }
+                    tempCoordinate++;
+                }
+                score *= tempScenicScore;
+
+                scenicScore.Add(score);
+            }
+        }
+
+        Console.WriteLine(scenicScore.Max());
+    }
+}
--- AoC2022/Program.cs
+++ AoC2022/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.WriteLine("Part 1, example data:");
-AoC2022.Day_07.part01(AoC2022.InputData.Day_07.ExampleData);
+AoC2022.Day_08.part01(AoC2022.InputData.Day_08.ExampleData);
 Console.WriteLine("Part 1, test data:");
-AoC2022.Day_07.part01(AoC2022.InputData.Day_07.TestData);
+AoC2022.Day_08.part01(AoC2022.InputData.Day_08.TestData);
 Console.WriteLine("Part 2, example data:");
-AoC2022.Day_07.part02(AoC2022.InputData.Day_07.ExampleData);
+AoC2022.Day_08.part02(AoC2022.InputData.Day_08.ExampleData);
 Console.WriteLine("Part 2, test data:");
-AoC2022.Day_07.part02(AoC2022.InputData.Day_07.TestData);
+AoC2022.Day_08.part02(AoC2022.InputData.Day_08.TestData);
